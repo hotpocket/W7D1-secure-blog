2b4cf7e22de4896504d6ef2da55bb5f1d4e9f864
hotpocket <githotpocket@gmail.com>
2024-04-20 03:05:47 -0700

    much happier w/ this rand function

--- registerUser.sh
+++ registerUser.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
 host="http://localhost:5000/api/user/register"
-outfile="response.json"
 
-# $1 = [a|n]  - Generate [n]umbers or [a]lphabet chars
-# $2 = number - The number of random characters to generate
-rand(){
-  # regex must be a variable so meta characters and literals are interpreted seperately
-  is_a_or_b='^[ab]$'
-  is_digit='^[0-9]+$'
-  if ! [[ $1 =~ $is_a_or_b ]] || ! [[ $2 =~ $is_digit ]]; then
-    echo "Usage: rand [a|n] <number of characters>"
-    return
+# if $1 has a dash, generate random alpha chars between those two ranges
+# else if $1 is a number, generate that many random alpha chars
+rand(){  # error checkins is lacking  ='(
+  genSize=$1
+  has_dash='^[0-9]+-[0-9]+$'
+  is_number='^[0-9]+$'
+  if [[ $genSize =~ $has_dash ]]; then
+    range_parts=(${1//-/ }); start=${range_parts[0]}; end=${range_parts[1]}
+    genSize=$((RANDOM % ($end - $start + 1) + $start)) # thank you copilot
   fi
-  [ "$1" == "a" ] && charset='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  [ "$1" == "n" ] && charset='0-9'
-  cat /dev/urandom |  tr -dc "$charset" | head -c$2
+  if ! [[ $genSize =~ $is_number ]]; then
+    echo "Usage: rand <number> | rand <start>-<end>"
+    return 1
+  fi
+  validChars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  cat /dev/urandom |  tr -dc "$validChars" | head -c$genSize
 }
 
 read -r -d '' postBodyData << HEREDOC_BLOCK
 {
-  "fullname" : "`rand a 7` `rand a 9`",
-  "email"    : "`rand a 7`@`rand a 12`.com",
-  "password" : "`rand a 14`"
+  "fullname" : "`rand 5-7` `rand 7-9`",
+  "email"    : "`rand 6-8`@`rand 8-14`.com",
+  "password" : "`rand 5-10`"
 }
 HEREDOC_BLOCK
 
